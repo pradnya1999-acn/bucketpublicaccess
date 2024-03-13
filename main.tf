a51af6259768fe5c75c3077535544ee293d009fe
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-03-13 17:44:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+#   version = "= 3.55.0"
+  credentials = var.sa_key
+  project = "parabolic-base-409505"
+}
+ 
+variable "sa_key" {
+  default =  ""
+}
+resource "google_storage_bucket" "auto-expire" {
+  name          = "no-public-access-bucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+
+   lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
